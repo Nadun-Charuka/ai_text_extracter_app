49b8a165a35dd15fc5e52e766e3adaa15c950aa4
Nadun Charuka <nadun.charuka2016@gmail.com>
2025-03-07 17:10:30 +0530

    bottom navigation done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:ai_text_extracter_app/constants/colors.dart';
 import 'package:ai_text_extracter_app/firebase_options.dart';
+import 'package:ai_text_extracter_app/main_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -16,7 +21,29 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "AI Text Extract app",
-      home: Scaffold(),
+      theme: ThemeData(
+        useMaterial3: true,
+        fontFamily: GoogleFonts.poppins().fontFamily,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          color: mainColor,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            elevation: 0,
+            padding: EdgeInsetsDirectional.symmetric(
+              vertical: 5,
+              horizontal: 20,
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+              side: BorderSide(color: mainColor),
+            ),
+            backgroundColor: mainColor,
+          ),
+        ),
+      ),
+      home: MainScreen(),
     );
   }
 }
---
+++ lib/main_screen.dart
@@ -0,0 +1,46 @@
+import 'package:ai_text_extracter_app/constants/colors.dart';
+import 'package:ai_text_extracter_app/screens/home_page.dart';
+import 'package:ai_text_extracter_app/screens/user_history.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  //Method to handle the tapping on bottom navigatin item
+  void _onTapItem(int index) {
+    debugPrint(index.toString());
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.transform),
+            label: "Conversion",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.history),
+            label: "History",
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onTapItem,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Color.fromARGB(255, 3, 42, 34),
+        backgroundColor: mainColor,
+      ),
+      body: _selectedIndex == 0 ? HomePage() : UserHistory(),
+    );
+  }
+}
