25bc45b587d397ff50fcdf4cb249f1a88b924ecf
Nadun Charuka <nadun.charuka2016@gmail.com>
2025-03-11 10:45:21 +0530

    model done

---
+++ lib/models/conversion_model.dart
@@ -0,0 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ConversionModel {
+  final String userId;
+  final String conversionData;
+  final DateTime conversionDate;
+
+  ConversionModel({
+    required this.userId,
+    required this.conversionData,
+    required this.conversionDate,
+  });
+
+  //json >> conversion model
+  //this is a factry name constructer
+  factory ConversionModel.fromjson(Map<String, dynamic> json) {
+    return ConversionModel(
+      userId: json["userId"],
+      conversionData: json["conversionData"],
+      conversionDate: (json["conversionDate"] as Timestamp).toDate(),
+    );
+  }
+
+  //conversionmodel >> json{}
+  Map<String, dynamic> toJson() {
+    return {
+      "userId": userId,
+      "conversionData": conversionData,
+      "conversionDate": conversionDate,
+    };
+  }
+}
---
+++ lib/services/store_conversions_firestore.dart
@@ -0,0 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class StoreConversionsFirestore {
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+
+  //save converstions
+  Future<void> storeConversionData(
+      {required conversionData, required conversionDate}) async {}
+}
